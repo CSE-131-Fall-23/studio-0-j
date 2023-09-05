f338c7d4d62a85079835ad7c4d3c71737650d11f
jeffreyhallett <31424548+jeffreyhallett@users.noreply.github.com>
2023-09-04 23:06:01 -0500

    Finished Studio 0

--- src/studio0/BurritoBudget.java
+++ src/studio0/BurritoBudget.java
@@ -8,11 +8,13 @@ public class BurritoBudget {
 		int burritosEatenWeek3 = 19; //don't judge me
 		int burritosEatenWeek4 = 1;
 		
-		int totalBurritos = burritosEatenWeek1 + burritosEatenWeek2 + burritosEatenWeek3 + burritosEatenWeek4;
-		double averageWeeklyBurritos = totalBurritos / 4;
-		
 		double burritoPrice = 7.50;
-		double averageWeeklyPrice = averageWeeklyBurritos * burritoPrice;
+		double week1Price = burritosEatenWeek1 * burritoPrice;
+		double week2Price = burritosEatenWeek2 * burritoPrice;
+		double week3Price = burritosEatenWeek3 * burritoPrice;
+		double week4Price = burritosEatenWeek4 * burritoPrice;
+		double totalPrice = week1Price + week2Price + week3Price + week4Price;
+		double averageWeeklyPrice = totalPrice / 4;
 		
 		System.out.println("You have spent an average of $" + averageWeeklyPrice + " per week on burritos.");
 	
